4d9aadc51d6bbedae3b3cdb8fc3a6d0e98e6fff4
brendon <brendon.soto@gmail.com>
2020-10-15 11:34:41 -0400

    feat: enabled drawing as a first step
    For the first step I wanted simple drawing capabilites:
    - the entire page is a canvas
    - click to put the pen down
    - move to draw
    - unclick/mouse up lifts the pen
    
    Also removed the confetti package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+/** GLOBALS **/
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let isDrawing = false;
+let canvasX = 0;
+let canvasY = 0;
+let canvasWidth = ctx.canvas.width;
+let canvasHeight = ctx.canvas.height;
+let canvasClientWidth = ctx.canvas.clientWidth;
+let canvasClientHeight = ctx.canvas.clientHeight;
 
-import confetti from 'canvas-confetti';
+/** EVENT LISTENERS **/
+canvas.addEventListener('mousedown', (e) => {
+  canvasX = (e.pageX / canvasClientWidth) * canvasWidth;
+  canvasY = (e.pageY / canvasClientHeight) * canvasHeight;
+  ctx.beginPath();
+  ctx.moveTo(canvasX, canvasY);
+  isDrawing = true;
+});
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+canvas.addEventListener('mouseup', (e) => {
+  ctx.closePath();
+  isDrawing = false;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+  if (!isDrawing) {
+    return;
+  }
+  canvasX = (e.pageX / canvasClientWidth) * canvasWidth;
+  canvasY = (e.pageY / canvasClientHeight) * canvasHeight;
+  ctx.lineTo(canvasX, canvasY);
+  ctx.stroke();
+});
