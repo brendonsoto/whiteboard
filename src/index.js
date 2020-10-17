304d857c71fef414f51defba4e5ad9b2e6e5b60e
brendon <brendon.soto@gmail.com>
2020-10-17 12:45:21 -0400

    feat: colour picker
    Currently only set it for pen, but tried to make it generic enough to be
    used anywhere
    When the colour bubble is clicked, a little menu appears above with a
    few preset colours. Once one is clicked, that is the active colour.
    Either the bubble can be clicked again or drawing triggers the popup to
    close.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
 /** GLOBALS **/
 // DOM hooks
-let canvas = document.getElementById('canvas');
-let penTool = document.getElementById('pen');
-let eraserTool = document.getElementById('eraser');
-let textTool = document.getElementById('text');
+const canvas = document.getElementById('canvas');
+const penTool = document.getElementById('pen');
+const eraserTool = document.getElementById('eraser');
+const textTool = document.getElementById('text');
+let colourPickerButtons = document.querySelectorAll('.colour');
+let colourChoices = document.querySelectorAll('.colour-choice');
+
+const tools = [penTool, eraserTool, textTool];
 
 // Canvas
-let ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
+const canvasWidth = ctx.canvas.width;
+const canvasHeight = ctx.canvas.height;
+const canvasClientWidth = ctx.canvas.clientWidth;
+const canvasClientHeight = ctx.canvas.clientHeight;
 let canvasX = 0;
 let canvasY = 0;
-let canvasWidth = ctx.canvas.width;
-let canvasHeight = ctx.canvas.height;
-let canvasClientWidth = ctx.canvas.clientWidth;
-let canvasClientHeight = ctx.canvas.clientHeight;
 
 // Functionality Flags
 let isPenDown = false;
 let isPenEnabled = false;
 
+/** HELPERS **/
+/**
+ * Unsets all other active tools and sets current tool
+ * @param {string} toolId - tool to set active
+ */
+const setActiveTool = (toolId) => {
+  tools.forEach((elem) => {
+    if (elem.id === toolId) {
+      elem.classList.add('active');
+    } else if (elem.classList.contains('active')) {
+      elem.classList.remove('active');
+    }
+  });
+};
+
 /** EVENT LISTENERS **/
 canvas.addEventListener('mousedown', (e) => {
+  // Close any active tool menus that may be open
+  document
+    .querySelectorAll('.colour-picker')
+    .forEach((elem) => elem.classList.add('hidden'));
+
+  /* NOTE The canvas has different dimensions than the browser window, so we're setting the correct coordinates here */
   canvasX = (e.pageX / canvasClientWidth) * canvasWidth;
   canvasY = (e.pageY / canvasClientHeight) * canvasHeight;
   ctx.beginPath();
@@ -50,12 +75,7 @@ penTool.addEventListener('click', (e) => {
   isPenEnabled = true;
   ctx.globalCompositeOperation = 'source-over';
 
-  // Set active class in tools
-  penTool.className = 'active';
-
-  // Unset any previous active class in tools
-  eraserTool.className = '';
-  textTool.className = '';
+  setActiveTool('pen');
 });
 
 eraserTool.addEventListener('click', (e) => {
@@ -64,22 +84,44 @@ eraserTool.addEventListener('click', (e) => {
   isPenEnabled = true;
   ctx.globalCompositeOperation = 'destination-out';
 
-  // Set active class in tools
-  eraserTool.className = 'active';
-
-  // Unset any previous active class in tools
-  penTool.className = '';
-  textTool.className = '';
+  setActiveTool('eraser');
 });
 
 textTool.addEventListener('click', (e) => {
   // Disable Pen
   isPenEnabled = false;
 
-  // Set active class in tools
-  textTool.className = 'active';
+  setActiveTool('text');
+});
 
-  // Unset any previous active class in tools
-  eraserTool.className = '';
-  penTool.className = '';
+/** COLOUR PIKCERS **/
+colourPickerButtons.forEach((elem) => {
+  elem.addEventListener('click', ({ target: { dataset } }) => {
+    document
+      .querySelector(`.colour-picker[data-tool=${dataset.tool}]`)
+      .classList.toggle('hidden');
+  });
+});
+
+colourChoices.forEach((elem) => {
+  elem.addEventListener('click', ({ target: { dataset } }) => {
+    console.log('COLOUR CLICKED', dataset.colour);
+
+    // Set the stroke colour
+    ctx.strokeStyle = dataset.colour;
+
+    // Set the colour of the corresponding colour picker button to the active colour
+    document.querySelector(
+      `.colour[data-tool=${dataset.tool}]`,
+    ).dataset.colour = dataset.colour;
+
+    // Set the active class
+    colourChoices.forEach((elem) => {
+      if (elem.dataset.colour === dataset.colour) {
+        elem.classList.add('active');
+      } else if (elem.classList.contains('active')) {
+        elem.classList.remove('active');
+      }
+    });
+  });
 });
